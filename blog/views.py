1218bf7e47060488aadccb0da2e736e266535178
Daniel <dan.email@example.com>
2024-09-20 15:58:31 +0600

    first commit

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
 context = {
-        'posts': [
-            {
-                'author': 'CoreyMS',
-                'title': 'Nurel is not Hacker',
-                'content': 'First post content',
-                'date_posted': 'August 27, 2018'
-            },
-            {
-                'author': 'Jane Doe',
-                'title': 'Daniel is genius',
-                'content': 'Second post content',
-                'date_posted': 'August 28, 2018'
-            }
-        ]
-    }
+    "posts" : Post.objects.all()
+}
+
+    
 
 def home(request):
     return render(request, 'blog/home.html', context)
