cd765ef22a97c6196e4442731872927f540e2e74
Demiurg20 <islamasylbekov1012@gmail.com>
2024-10-02 13:57:32 +0600

    Django Main True

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # my apps
-    'blog'
+    'blog',
+    'users',
+    'crispy_forms',
+    'crispy_bootstrap4',
+
+
 ]
 
 MIDDLEWARE = [
@@ -118,8 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+
+
--- django_project/urls.py
+++ django_project/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
+from users.views import register
+from django.contrib.auth import views as auth_views
+from users import views as users_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('profile/', users_views.Profile, name='Profile'),
+    path('register/', register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'logout.html'), name='logout'),
 ]
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
