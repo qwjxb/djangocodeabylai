d70a226e3ad2f45c5daafdb59655becd921fa2f3
Demiurg20 <islamasylbekov1012@gmail.com>
2024-10-24 11:47:38 +0600

    Islam blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
@@ -15,10 +16,25 @@ context = {
 def home(request):
     return render(request, 'blog/home.html', context)
 
-class PostListView (ListView):
+class PostListView(ListView):
     model = Post
-    template_name = "blog/home.html"
+    template_name = "post_list.html"
     context_object_name = 'posts'
+    ordering = ['-date_posted']  # Sorting by date posted
+    paginate_by = 5  # Paginate by 5 posts per page
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')  # Get the search query
+        if query:
+            return Post.objects.filter(title__icontains=query) | Post.objects.filter(content__icontains=query)
+        else:
+            return Post.objects.all().order_by('-date_posted')  # Return all posts, ordered by date
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q', '')  # Add the query to context for form re-population
+        return context
+
 
 
 class PostDetailView(DetailView):
@@ -53,6 +69,17 @@ class PostUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         if self.request.user == post.author:
             return True
         return False   
+    
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')  # Сортируем посты по дате создания
+
+    # Настройка пагинации (показываем 1 постов на странице)
+    paginator = Paginator(posts, 1)  # 1 постов на каждой странице
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'post_list.html', {'page_obj': page_obj})
+
 
 
         
